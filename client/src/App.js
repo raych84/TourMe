be23804548ea36573af0bd0401273cb3b62f5889
Rachel Williams <rachel_wms@hotmail.com>
2020-11-16 16:27:21 -0500

    added images to use for hero.

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react"; 
+import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import Navbar from "./components/Navbar";
@@ -11,17 +11,17 @@ import SavedTours from "./pages/SavedTours";
 
 function App() {
   return (
-   <Router>
-     <div>
-       <Navbar />
-       <Route exact path = "/" component= {Home} />
-       <Route exact path = "/home" component = {Home} />
-       <Route exact path = "/signup" component = {Signup} />
-       <Route exact path = "/login" component = {Login} />
-       <Route exact path = "/savedtours" component = {SavedTours}/>
-       <Footer />
-     </div>
-   </Router>
+    <Router>
+      <div>
+        <Navbar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/signup" component={Signup} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/savedtours" component={SavedTours} />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- client/src/components/hero.js
+++ client/src/components/hero.js
@@ -1,12 +1,24 @@
 import React from "react";
 
-
-function Hero(props) {
+function Hero() {
   return (
-    <div className="hero text-center" style={{ backgroundImage: `url(${props.backgroundImage})` }}>
-      {props.children}
+
+    
+    <div id="carouselExampleSlidesOnly" className="carousel slide" data-ride="carousel">
+    <div className="carousel-inner">
+      <div className="carousel-item active">
+        <img src="../images/RioLondoneiro.jpg" className="d-block w-100" alt="Rio Londoneiro" />
+      </div>
+      <div className="carousel-item">
+        <img src="../images/Dubaris.jpg" className="d-block w-100" alt="Dubaris" />
+      </div>
+      <div className="carousel-item">
+        <img src="../images/Moscanbul.jpg" className="d-block w-100" alt="Moscanbul" />
+      </div>
     </div>
+  </div>
   );
 }
+
 
 export default Hero;
